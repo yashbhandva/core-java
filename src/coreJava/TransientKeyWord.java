33ef8f03acf592a21db5a772bffee6acd993c758
yashbhandva <yashbhandva01@gmail.com>
2024-07-26 00:43:48 +0530

    Update TransientKeyWord.java

--- src/coreJava/TransientKeyWord.java
+++ src/coreJava/TransientKeyWord.java
@@ -2,11 +2,11 @@ package coreJava;
 
 import java.io.*;
 
-class Mail implements Serializable{
+class Mail implements Serializable {
     private String id;
     transient private int password;
 
-    Mail(String id,int password){
+    Mail(String id, int password) {
         this.id = id;
         this.password = password;
     }
@@ -19,9 +19,10 @@ class Mail implements Serializable{
         return password;
     }
 }
+
 public class TransientKeyWord {
-    public static void main(String[] args) throws IOException,ClassNotFoundException {
-        Mail mail = new Mail("xyz@gmail.com",95125);
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Mail mail = new Mail("xyz@gmail.com", 95125);
         FileOutputStream fo = new FileOutputStream("C:\\Users\\Dell\\Desktop\\xyz\\yas.txt");
         ObjectOutputStream o = new ObjectOutputStream(fo);
         o.writeObject(mail);
@@ -33,7 +34,7 @@ public class TransientKeyWord {
         ObjectInputStream o1 = new ObjectInputStream(fi);
         Mail mail1 = (Mail) o1.readObject();
 
-        System.out.println("Mail id is : "+mail1.getId());
-        System.out.println("password is : "+mail1.getPassword());
+        System.out.println("Mail id is : " + mail1.getId());
+        System.out.println("password is : " + mail1.getPassword());
     }
 }
