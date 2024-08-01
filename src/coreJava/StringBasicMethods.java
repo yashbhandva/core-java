0caefc559c6e89042413d478096f85bca7e4c048
yashbhandva <yashbhandva01@gmail.com>
2024-08-02 00:03:27 +0530

    Update StringBasicMethods.java

--- src/coreJava/StringBasicMethods.java
+++ src/coreJava/StringBasicMethods.java
@@ -1,5 +1,18 @@
 package coreJava;
-
+class basics{
+    public void St(){
+        String s = "madara";
+        s+=" uchiha";
+        s+= 'G';
+        s+=10;
+        System.out.println(s);
+    }
+    public void St2(){
+        String s = "hello";
+        String x = s.substring(0,2) + 'y' + s.substring(3);
+        System.out.println(x);
+    }
+}
 public class StringBasicMethods {
     public static void main(String[] args) {
         String s = "madara  ";
@@ -15,5 +28,11 @@ public class StringBasicMethods {
         /*(10)*/ System.out.println(s.toUpperCase());
         /*(11)*/ System.out.println(s.trim());
         /*(12)*/ System.out.println(s.indent(0));
+        System.out.println();
+        System.out.println("madara"+10+20);
+        System.out.println(10+20+"madara");
+        basics b = new basics();
+        b.St();
+        b.St2();
     }
 }
