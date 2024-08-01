cc2ed94071af0f8e21b6152a2847ea360c53508c
yashbhandva <yashbhandva01@gmail.com>
2024-08-02 00:03:23 +0530

    Update StringBuilders.java

--- src/coreJava/StringBuilders.java
+++ src/coreJava/StringBuilders.java
@@ -3,8 +3,13 @@ package coreJava;
 public class StringBuilders {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("Itachi");
-        System.out.println(sb.charAt(0));
+        sb.setCharAt(0,'g'); //-->gtachi
+        sb.append(10); //--> gtachi10
+        sb.insert(1,'i'); // --> gitachi10
+        //sb.delete(0,7); -->10
+        sb.deleteCharAt(0); //-->itachi10
         System.out.println(sb);
-
+        sb.reverse();
+        System.out.println(sb);
     }
 }
