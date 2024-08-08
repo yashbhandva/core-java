23f05c60c3a5640d50b52a6cef29b6c5e6dc9445
yashbhandva <yashbhandva01@gmail.com>
2024-08-08 23:18:29 +0530

    Create RuntimeMethod.java

---
+++ src/RuntimeMethod.java
@@ -0,0 +1,8 @@
+public class RuntimeMethod {
+    public static void main(String[] args) {
+        Runtime e = Runtime.getRuntime();
+        long b = e.totalMemory();
+        long c = e.freeMemory();
+        System.out.println(b+" : "+c);
+    }
+}
