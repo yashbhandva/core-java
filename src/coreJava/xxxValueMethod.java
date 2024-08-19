7ec89a95495eec5ebb8bf39099659fc199ce6ea0
yashbhandva <yashbhandva01@gmail.com>
2024-08-19 23:49:16 +0530

    Create xxxValueMethod.java

---
+++ src/coreJava/xxxValueMethod.java
@@ -0,0 +1,13 @@
+package coreJava;
+
+public class xxxValueMethod {
+    public static void main(String[] args) {
+        Integer i = new Integer(130);
+        System.out.println(i.byteValue());
+        System.out.println(i.shortValue());
+        System.out.println(i.intValue());
+        System.out.println(i.longValue());
+        System.out.println(i.floatValue());
+        System.out.println(i.doubleValue());
+    }
+}
