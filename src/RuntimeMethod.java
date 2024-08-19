47ce8506b848217528b7011a6c70a8a03aa652c5
yashbhandva <yashbhandva01@gmail.com>
2024-08-19 23:49:00 +0530

    Delete RuntimeMethod.java

--- src/RuntimeMethod.java
+++
@@ -1,8 +0,0 @@
-public class RuntimeMethod {
-    public static void main(String[] args) {
-        Runtime e = Runtime.getRuntime();
-        long b = e.totalMemory();
-        long c = e.freeMemory();
-        System.out.println(b+" : "+c);
-    }
-}
