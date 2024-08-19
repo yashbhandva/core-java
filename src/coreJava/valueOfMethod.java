3e7e6eb293e2d41ebaf847de0e1bd1a87fe40a0a
yashbhandva <yashbhandva01@gmail.com>
2024-08-19 23:49:21 +0530

    Create valueOfMethod.java

---
+++ src/coreJava/valueOfMethod.java
@@ -0,0 +1,10 @@
+package coreJava;
+
+public class valueOfMethod {
+    public static void main(String[] args) {
+        Integer i = Integer.valueOf("1000");
+        System.out.println(i);
+        Float f = Float.valueOf("100");
+        System.out.println(f);
+    }
+}
