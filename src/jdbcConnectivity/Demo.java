cc22f772e66b0bda4477c5e4db1dc9c8ed210898
yashbhandva <yashbhandva01@gmail.com>
2024-08-19 23:48:25 +0530

    Create Demo.java

---
+++ src/jdbcConnectivity/Demo.java
@@ -0,0 +1,7 @@
+package jdbcConnectivity;
+
+public class Demo {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+    }
+}
