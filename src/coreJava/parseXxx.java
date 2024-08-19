53cf58835bf6c8305dc5f25acd2a71e866e0a9ce
yashbhandva <yashbhandva01@gmail.com>
2024-08-19 23:49:32 +0530

    Create parseXxx.java

---
+++ src/coreJava/parseXxx.java
@@ -0,0 +1,13 @@
+package coreJava;
+
+public class parseXxx {
+    public static void main(String[] args) {
+        int i = Integer.parseInt("10");
+        boolean b = Boolean.parseBoolean("true");
+        double d  = Double.parseDouble("10.5");
+
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(d);
+    }
+}
