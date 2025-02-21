57f098e646dfa3a3915f306ee9af24f65b7ed784
yashbhandva <yashbhandva01@gmail.com>
2025-02-21 23:23:51 +0530

    Update JFrames.java

--- src/advanceJava/JFrames.java
+++ src/advanceJava/JFrames.java
@@ -1,16 +1,12 @@
 package advanceJava;
 
-import javax.swing.*;
-import java.awt.*;
 
+import javax.swing.*;
 public class JFrames {
     public static void main(String[] args) {
-        JFrame jFrame = new JFrame();
-        jFrame.setSize(420,420);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setResizable(false);
-        jFrame.setTitle("uchiha Itachi");
-        jFrame.setVisible(true);
-        jFrame.getContentPane().setBackground(Color.cyan);
+        JFrame frame = new JFrame("cleck here");
+        frame.setTitle("this is a first frame");
+        frame.setSize(420,420);
+        frame.setVisible(true);
     }
 }
