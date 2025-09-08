66877a44fd0208921add091e8eed8cfc0f5954e7
yashbhandva <yashbhandva01@gmail.com>
2025-09-08 21:42:46 +0530

    Create MinMaxSecondLargest.java

---
+++ src/InterviewQuestions/MinMaxSecondLargest.java
@@ -0,0 +1,21 @@
+package InterviewQuestions;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class MinMaxSecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,0};
+
+        String result = Arrays.
+                    stream(arr).
+                    mapToObj(String::valueOf).
+                    collect(Collectors.joining(", "));
+
+        System.out.println(result);
+
+        int len = result.length();
+        
+    }
+}
