8b6648e3ff9f910103c83801fccb924d56481fdc
yashbhandva <yashbhandva01@gmail.com>
2025-09-08 21:42:04 +0530

    Create StringIntChar.java

---
+++ src/InterviewQuestions/StringIntChar.java
@@ -0,0 +1,11 @@
+package InterviewQuestions;
+
+public class StringIntChar {
+    public static void main(String[] args) {
+        String s = "yash";
+        s = s+10;
+        s = s+" bhandva";
+
+        System.out.println(s);
+    }
+}
